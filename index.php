9d26f0960c630f3a040887fc372e3ddb71c6fb9e
18OJohnston.ACC <18ojohnston.acc@lwetb.ie>
2023-03-06 11:56:03 +0000

    bob

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<script>
+			function validate() {
+				var requiredElements = ["firstname", "surname", "email", "password", "passwordConfirm", "addr1", "addr2", "eircode"]
+				var success = true
+
+				requiredElements.forEach((a) => {
+					if (document.getElementById(a).value == "") {
+						success = false	
+					}
+				})
+
+				if (!success) {
+					alert("Some input fields are unfilled")
+					return false
+				}
+
+				
+				if (document.querySelector('input[name="gender"]:checked')===null) {
+					alert("Select a gender")
+					return false
+				}
+
+				if (document.getElementById("password").value !== document.getElementById("passwordConfirm").value) {
+					alert("Passwords don't match")
+					return false
+				}
+				
+				document.getElementById('userform').addEventListener('formdata', (e) => {
+					var formData = e.formData;
+					form.set('gender', document.querySelector('input[name="gender"]:checked').id)
+				});
+
+				return true
+			}
+		</script>
+	</head>
+	<body>
+		<form action="submit.php" method="post" onsubmit="return validate()" id="userform">
+			<label for="firstname">First Name</label><br/>
+			<input type="text" id="firstname" maxlength="50" name="firstname"><br/>
+			
+			<label for="surname">Second Name</label><br/>
+			<input type="text" id="surname" maxlength="50" name="surname"><br/>
+			
+			<label for="email">Email</label><br/>
+			<input type="text" id="email" maxlength="320" name="email"><br/>
+			
+			<label for="password">Password:</label><br/>
+			<input type="password" id="password" maxlength="64" name="password"><br/>
+			
+			<label for="passwordConfirm">Confirm Password:</label><br/>
+			<input type="password" id="passwordConfirm"><br/>
+
+			<fieldset>
+				<legend>Gender:</legend>
+				<input type="radio" id="male" name="gender">
+				<label for="male">Male</label>
+				
+				<input type="radio" id="female" name="gender">
+				<label for="female">Female</label>
+
+				<input type="radio" id="other" name="gender">
+				<label for="other">Other</label>
+			</fieldset>
+			<label for="addr1">Address Line 1:</label><br/>
+			<input type="text" id="addr1" name="addr1"><br/>
+			<label for="addr2">Address Line 2:</label><br/>
+			<input type="text" id="addr2" name="addr2"><br/>
+			<label for="eircode">Eircode:</label><br/>
+			<input type="text" id="eircode" maxlength="7" name="eircode"><br/>
+			<button type="submit">Submit</button><br/>
+		</form>	
+	</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+		<?php
+		function validate_input($data) {
+			$data = trim($data); // Remove unnecessary whitespace
+			$data = stripslashes($data); // Remove unnec
+			$data = htmlspecialchars($data); // convert special characters to html
+
+			return $data;
+		}
+
+		print_r($_POST);
+		// Check all fields have values
+		$fields = ['firstname', 'surname', 'email', 'password', 'addr1', 'addr2', 'eircode', 'gender'];
+		$values = array();
+
+
+		foreach($fields as &$field) {
+			if(!empty($_POST[$field])) {
+				$values[$field]=validate_input($_POST[$field]);
+			} else {
+				echo "Didn't find symbol $field ";
+				exit();
+			}
+		}
+		$host = "localhost";
+		$username = "OJ";
+		$password = "password";
+		$database = "oj_database";
+		$port = 3307;
+		$conn = mysqli_connect($host, $username, $password, $database, $port);
+		if (mysqli_connect_errno()) {
+			echo "Failed to connect to database: ".mysqli_connect_error()."<br/>";
+			exit();
+		} else {
+			echo "Connected Successfully";
+		}
+		$sql = "
+			DROP TABLE details;
+			CREATE TABLE IF NOT EXISTS details (
+				id INT AUTO_INCREMENT PRIMARY KEY,
+				firstname CHAR(50),
+				lastname CHAR(50),
+				gender ENUM('male', 'female', 'other'),
+				email CHAR(255),
+				password CHAR(64),
+				address_1 VARCHAR(80),
+				address_2 VARCHAR(80),
+				eircode CHAR(7)
+			);
+		";
+		if (mysqli_query($conn, $sql)) {
+			echo "Created table successfully <br/>";
+		} else {
+			echo "Failed to create the table <br/>";
+		}
+
+		$sql = "INSERT INTO TABLE details (firstname, lastname, gender, password, address_1, address_2, eircode) VALUES(".$values['firstname'].", ".$values['surname'].", ".$values['gender'].", ".$values['password'].", ".$values['addr1'].", ".$values['addr2'].", ".$values['eircode'].")";
+
+		if (mysqli_query($conn,$sql)){ 
+			echo "Inserted Data Successfully <br/>";
+		} else {
+				echo "Failed to insert data<br/>";
+		}
+		mysqli_close($conn);
+ 		?>
+	</body>
+</html>
